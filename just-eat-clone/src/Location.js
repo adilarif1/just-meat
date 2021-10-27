04186b382af2d96702802a2a4b8036c1c92f3816
Adil Arif <adilarif1@icloud.com>
2021-10-27 22:47:14 +0100

    Testing useState hooks to display lat and long

--- just-eat-clone/src/Location.js
+++ just-eat-clone/src/Location.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
+const Latlong = () => {
 
-const Latlong = props => {
-
-
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(function(position) {
               
-        
+          // Declare a new state variable, which we'll call "count"
+        setCount(position.coords.latitude)
+        setCount2(position.coords.longitude)
+        //count = position.coords.latitude;
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
        
-        return <h1>Location is: {props.Latlong} </h1>
+        return <h1>Location is: {count} </h1>
 
           
         });
         
-      }	
+      }
 
-      return <h1>Location is: {props.Latlong} </h1>
+      
+      return <h1>Location is: {count} + {count2} </h1>
 
 
 
