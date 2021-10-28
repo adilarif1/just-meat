8ee4954883a80228a20846895c15314dd2dab790
Adil Arif <adilarif1@icloud.com>
2021-10-28 23:13:53 +0100

    axios added

--- just-eat-clone/src/Location.js
+++ just-eat-clone/src/Location.js
@@ -1,31 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"; 
+
 
 const Latlong = () => {
 
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const [data, setData] = useState("");
+  
+  
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(function(position) {
               
           // Declare a new state variable, which we'll call "count"
-        setCount(position.coords.latitude)
-        setCount2(position.coords.longitude)
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
         //count = position.coords.latitude;
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
        
-        return <h1>Location is: {count} </h1>
+        //return <h1>Location is: {lat}  +  {long}</h1>
 
+     
           
         });
         
       }
 
+      useEffect(() => {
+        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR-API').then((Response) => {
+          
+          setData(Response.data);
+        
+          });
+        
+          //return <h1>Location is: {lat}  +  {long}  </h1>
+        
+        }, []);
       
-      return <h1>Location is: {count} + {count2} </h1>
+        return <h1>Location is: {data}</h1>
 
 
 
 }
 
-export default Latlong
+export default Latlong;
