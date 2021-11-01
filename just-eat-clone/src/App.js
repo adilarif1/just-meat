1c08dabd0af6cc69f300f4e751aa77f2a77756ab
Adil Arif <adilarif1@icloud.com>
2021-11-01 00:26:21 +0000

    “update”

--- just-eat-clone/src/App.js
+++ just-eat-clone/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Location from './Location';
+import Table from './Table';
 
 function App(){
   return (
@@ -18,7 +19,7 @@ function App(){
         >
           Learn React 
         </a>
-		  
+		  <Table/>
         <Location/>
       </header>
     </div>
