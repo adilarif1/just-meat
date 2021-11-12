1d24bfc263429f31235c25d56405799640ce9909
Adil Arif <adilarif1@icloud.com>
2021-11-12 21:07:28 +0000

    mergg

--- just-eat-clone/src/App.js
+++ just-eat-clone/src/App.js
@@ -1,20 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
-import Location from './Location';
-import Table from './Table';
-import Landing from './Landing';
+//import Location from './Location';
+//import Table from './Table';
+//import Landing from './Landing';
+import Login from './Login/Login';
+import Dashboard from './Dashboard/Dashboard';
+import Preferences from './Preferences/Preferences';
+
+
 
 function App(){
+  const [token, setToken] = useState();
+  
+
+   if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-       
-        <Landing/>
-		    <Table/>
-       
-      </header>
+   <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
